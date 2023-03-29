cb86e56de63b2964ba5199074281968e2fe753ca
fenix.sol <jiangfengming87@gmail.com>
2023-03-29 23:25:53 +0800

    passing down props by {..$$restProps}

--- index.js
+++ index.js
@@ -7,29 +7,20 @@ const defaultConfigFile = path.join(__dirname, 'svgo.config.js');
 function convert(svg, ts) {
   const script = ts
     ? `<script lang="ts">
-  let className: string | undefined = undefined;
-
-  export { className as class };
-  export let width: number | string | undefined = undefined;
-  export let height: number | string | undefined = undefined;
+  export let width: number | string | undefined;
+  export let height: number | string | undefined;
   export let fill = 'currentColor';
-  export let style: string | undefined = undefined;
 </script>`
     : `<script>
-  let className = undefined;
-
-  export { className as class };
-  export let width = undefined;
-  export let height = undefined;
+  export let width;
+  export let height;
   export let fill = 'currentColor';
-  export let style = undefined;
 </script>`;
 
+  const p = svg.indexOf('>');
+
   return script + '\n\n' +
-    svg.replace(
-      '>',
-      ' {style} class={className} {width} height={!width && !height ? \'1em\' : height} {fill} on:click on:keydown on:keyup>'
-    );
+    svg.slice(0, p) + ' {width} height={!width && !height ? \'1em\' : height} {fill} {...$$restProps} on:click on:keydown on:keyup>' + svg.slice(p + 1);
 }
 
 module.exports = async function(input, output, ts, configFile = defaultConfigFile) {
